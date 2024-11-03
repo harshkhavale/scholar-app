f582a1f31dac946dad3007807ad12fed2e57d8ea
Harsh <harshkhavale1102@gmail.com>
2024-11-03 09:00:50 +0530

    init

---
+++ app/index.tsx
@@ -0,0 +1,18 @@
+import { useRef, useEffect } from 'react';
+import { Text, View } from 'react-native';
+import LottieView from 'lottie-react-native';
+
+export default function App() {
+  useEffect(() => {
+    
+  }, []);
+
+  return (
+    <View className='bg-orange-400 h-screen flex justify-center items-center'>
+     <Text className='flex justify-center'>Welcome scholar!</Text>
+     
+    </View>
+  );
+}
+
+
