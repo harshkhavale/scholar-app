ede16a5fb8cb0601ec788fa011c23052a63c5f52
harshkhavale <harshkhavale1102@gmail.com>
2024-12-08 17:19:01 +0530

    stable-version

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,11 @@
-const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require('nativewind/metro');
+// Learn more https://docs.expo.io/guides/customizing-metro
+const { getDefaultConfig } = require('expo/metro-config');
 
-const config = getDefaultConfig(__dirname)
+/** @type {import('expo/metro-config').MetroConfig} */
+const config = getDefaultConfig(__dirname, {
+    // Disable CSS support.
+    isCSSEnabled: true,
 
+  });
 module.exports = withNativeWind(config, { input: './global.css' })
